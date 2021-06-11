2d04b27cf1f86ccdd705dd24e83cc64b69a28afe
Jugesh Raghav <jugesh1520raghav@gmail.com>
2021-06-11 14:28:47 +0530

    the drum can be played with the keyboard keys.

--- Band/app.js
+++ Band/app.js
@@ -19,6 +19,41 @@ window.addEventListener('load', ()=>{
         });
     });
 
+    //the drum can be played with the keys A.S,D,J,K,L
+    document.addEventListener('keypress', function(event) {
+        var keyPressed = event.key;
+        switch(keyPressed){
+            case 'w':
+               var crash = new Audio('sounds/crash.mp3');
+               crash.play();  
+               break;
+           case 'a':
+               var kick = new Audio('sounds/kick-bass.mp3');
+               kick.play();  
+               break;
+           case 's':
+               var snare = new Audio('sounds/snare.mp3');
+               snare.play();  
+               break;
+           case 'd':
+               var tom_1 = new Audio('sounds/tom-1.mp3');
+               tom_1.play(); 
+               break;
+           case 'j':
+                var tom_2 = new Audio('sounds/tom-2.mp3');
+                tom_2.play();  
+                break;
+           case 'k':
+               var tom_3 = new Audio('sounds/tom-3.mp3');
+               tom_3.play();  
+               break;
+            case 'l':
+               var tom_4 = new Audio('sounds/tom-4.mp3');
+               tom_4.play();  
+               break;
+        }
+     })
+
     const createBubbles = (index) =>{
         const bubble = document.createElement('div');
         visual.appendChild(bubble);
