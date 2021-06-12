d0516b1086c01cc4415b10d1a454cae107579f9e
ShwetKhatri2001 <19dcs001@lnmiit.ac.in>
2021-06-12 11:42:55 +0530

    fixes on dance page

--- Dance/script.js
+++ Dance/script.js
@@ -1,16 +1,3 @@
-// YOU WILL NEED TO ADD YOUR OWN API KEY IN QUOTES ON LINE 5, EVEN FOR THE PREVIEW TO WORK.
-// 
-// GET YOUR API HERE https://console.developers.google.com/apis/api
-
-
-// https://developers.google.com/youtube/v3/docs/playlistItems/list
-
-// https://console.developers.google.com/apis/api/youtube.googleapis.com/overview?project=webtut-195115&duration=PT1H
-
-// <iframe width="560" height="315" src="https://www.youtube.com/embed/qxWrnhZEuRU" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-
-// https://i.ytimg.com/vi/qxWrnhZEuRU/mqdefault.jpg
-
 
 $(document).ready(function () {
 
