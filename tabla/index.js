e223ed70f201c0f8cbbf38ea208f5e5bce380a83
nisha <nishayadav2548706@gmail.com>
2021-06-13 12:38:03 +0530

    tabla

--- tabla/index.js
+++ tabla/index.js
@@ -1,10 +1,10 @@
 
 document.querySelectorAll("button")[0].addEventListener("click",function (){
-	var audio=new Audio("sound/t.mp3.");
+	var audio=new Audio("sound/t.mp3");
 	audio.play();
 })
 document.querySelectorAll("button")[1].addEventListener("click",function (){
-	var audio=new Audio("sound/d.mp3.");
+	var audio=new Audio("sound/d.mp3");
 	audio.play();
 })
 document.addEventListener("keypress",function (event){
