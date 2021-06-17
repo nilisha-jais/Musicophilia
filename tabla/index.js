22b765d112278457c18a7ff3d33dcc8d718b890b
nisha <nishayadav2548706@gmail.com>
2021-06-17 16:43:09 +0530

    added keypress

--- tabla/index.js
+++ tabla/index.js
@@ -7,3 +7,15 @@ document.querySelector(".d").addEventListener("click",function (){
 	var audio=new Audio("sound/d.mp3");
 	audio.play();
 })
+var audioa = new Audio("sound/t.mp3");
+var audiob = new Audio("sound/d.mp3");
+window.addEventListener("keydown", checkKeyPress, false);
+
+function checkKeyPress(key) {
+ if (key.keyCode == "84") { 
+    audioa.play();
+ }
+ if(key.keyCode == "68"){ 
+    audiob.play();
+ }
+}
