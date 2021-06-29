53585d5202f98acf2eb30bd55e2ca95db9f0b8d3
SanjuktaSunam <sanjukta16sunam@gmail.com>
2021-06-30 01:10:00 +0530

    Added and implemented sounds

--- tabla/index.js
+++ tabla/index.js
@@ -7,8 +7,24 @@ document.querySelector(".d").addEventListener("click",function (){
 	var audio=new Audio("sound/d.mp3");
 	audio.play();
 })
+document.querySelector(".u").addEventListener("click",function (){
+	var audio=new Audio("sound/u.m4a");
+	audio.play();
+})
+document.querySelector(".w").addEventListener("click",function (){
+	var audio=new Audio("sound/w.m4a");
+	audio.play();
+})
+document.querySelector(".y").addEventListener("click",function (){
+	var audio=new Audio("sound/y.m4a");
+	audio.play();
+})
 var audioa = new Audio("sound/t.mp3");
 var audiob = new Audio("sound/d.mp3");
+var audioc = new Audio("sound/u.m4a");
+var audiod = new Audio("sound/w.m4a");
+var audioe = new Audio("sound/y.m4a");
+
 window.addEventListener("keydown", checkKeyPress, false);
 
 function checkKeyPress(key) {
@@ -17,5 +33,14 @@ function checkKeyPress(key) {
  }
  if(key.keyCode == "68"){ 
     audiob.play();
+ }
+ if (key.keyCode == "85") { 
+    audioc.play();
+ }
+ if(key.keyCode == "87"){ 
+    audiod.play();
+ }
+ if(key.keyCode == "89"){ 
+    audioe.play();
  }
 }
